0e86b4b0b8cf820bd56e86bf6a3158c6dece06d7
chovaiv <chovaiv@gmail.com>
2022-10-31 19:16:50 +0100

    Initial commit

---
+++ graf/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from colorfield.fields import ColorField
+
+class Test(models.Model):
+    jmeno_objektu = models.CharField(max_length=200)
+    pocet_objektu = models.IntegerField()
+    color = ColorField(default='#000000')
+    borderColor = ColorField(default='#FF00FF')
+
+
+
+    def __str__(self):
+        return "{}-{}".format(self.jmeno_objektu, self.pocet_objektu)
---
+++ graf/views.py
@@ -0,0 +1,50 @@
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import TemplateView
+from .models import Test
+from django.template import loader
+
+class TestChartView(TemplateView):
+    template_name = 'graf/doughnut.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["qs"] = Test.objects.all()
+        return context
+
+def pridat(request):
+  template = loader.get_template('graf/upravit.html')
+  return HttpResponse(template.render({}, request))
+
+
+def pridat_objekt(request):
+  x = request.POST['jmeno_objektu']
+  y = request.POST['pocet_objektu']
+  z = request.POST['color']
+  w = request.POST['borderColor']
+  test = Test(jmeno_objektu = x, pocet_objektu = y, color = z, borderColor = w)
+  test.save()
+  return HttpResponseRedirect(reverse('index'))
+
+
+def odstranit(request, id):
+  test = Test.objects.get(id=id)
+  test.delete()
+  return HttpResponseRedirect(reverse('index'))
+
+def upravit(request, id):
+  test = Test.objects.get(id=id)
+  context = {
+    'test': test,
+  }
+  return HttpResponseRedirect(reverse('index'))
+
+
+def upravit_objekt(request, id):
+  jmeno_objektu = request.POST['jmeno_objektu']
+  pocet_objektu = request.POST['pocet_objektu']
+  test = Test.objects.get(id=id)
+  test.jmeno_objektu = jmeno_objektu
+  test.pocet_objektu = pocet_objektu
+  test.save()
+  return HttpResponseRedirect(reverse('index'))
