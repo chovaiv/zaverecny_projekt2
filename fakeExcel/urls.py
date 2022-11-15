47c1ada645264cff38788b3ddbfe1e21ce201a26
chovaiv <60153233+chovaiv@users.noreply.github.com>
2022-11-15 12:49:48 +0100

    Update urls.py

--- fakeExcel/urls.py
+++ fakeExcel/urls.py
@@ -1,18 +1,3 @@
-"""fakeExcel URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
 
@@ -21,8 +6,8 @@ from graf.views import TestChartView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TestChartView.as_view(), name='index'),
-    path('upravit/', views.pridat, name='upravit'),
+    path('piechart/', TestChartView.as_view(), name='piechart'),
+    path('', views.index, name='index'),
     path('pridat_objekt/', views.pridat_objekt, name='pridat_objekt'),
     path('odstranit/<int:id>/', views.odstranit, name='odstranit'),
     path('upravit_objekt/<int:id>', views.upravit_objekt, name='upravit_objekt'),
